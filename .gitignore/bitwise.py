a161aa8a2818eac59c5e9ba62e52381e922e7b9e
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-07 16:55:18 +0530

    bitwise operator

---
+++ .gitignore/bitwise.py
@@ -0,0 +1,10 @@
+try:
+    x=int(input("enter first value"))
+    y=int(input("enter second value"))
+    x=x^y
+    y=x^y
+    x=x^y
+    print(x,y,("swaaped nums"))
+except:
+    print('invalid')
+    print('invalid')
